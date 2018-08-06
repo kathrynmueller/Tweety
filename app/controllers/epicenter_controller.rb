dfd9b9a5d32a0b8fde7d6e05b8e1030430326179
Kathryn Mueller <kathrynmueller@gmail.com>
2018-08-06 10:22:26 -0400

    fixed feed for users with no tweets

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -8,7 +8,7 @@ class EpicenterController < ApplicationController
   def feed
     @following_tweets = Tweet.where(user_id: current_user.following.push(current_user.id))
                           .order(created_at: :desc).page(params[:page]).per(5)
-    @users = User.all
+    @users_with_tweets = User.where.not(name: [nil, ""])
     @tags = Tag.all
   end
 
